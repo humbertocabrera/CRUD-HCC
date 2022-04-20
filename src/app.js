b4e91cf389964712b69ddfc6cd507370daedac38
humbertocabrera <humbertoccavazos@gmail.com>
2022-04-20 14:59:04 -0500

    First Commit

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const mysql = require('mysql');
+const myConnection = require('express-myConnection');
+
+const app = express();
+
+//importing routes
+const customerRoutes = require('./routes/customer');
+
+//settings
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//middlewares
+app.use(morgan('dev'));
+app.use(myConnection(mysql,{
+    host:'sql5.freemysqlhosting.net',
+    user:'sql5469719',
+    password:'cT64GAQfAL',
+    port: 3306,
+    database:'sql5469719'
+},'single'))
+app.use(express.urlencoded({extended: false}));
+
+//routes
+app.use('/', customerRoutes);
+
+app.listen(app.get('port'), ()=>{
+    console.log('Server on port 3000');
+});
